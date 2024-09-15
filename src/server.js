a3c8ec24a59a934f2f5e004ba2f73f2ed0b55a4b
Aniket <aniket@itobuz.com>
2024-09-15 17:06:12 +0530

    cors config for frontend

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import { connectToMongoDB } from "./connection/db.js";
 import { config } from "./config/config.js";
 import rootRouter from "./router/rootRouter.js";
@@ -8,6 +9,11 @@ connectToMongoDB();
 const app = express();
 const port = config.PORT;
 app.use(express.json());
+
+// Enable CORS
+app.use(cors({
+  origin: 'http://localhost:5173', // Allow your frontend domain
+}));
 
 app.use(rootRouter);
 
